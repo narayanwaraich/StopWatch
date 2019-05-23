41a95764f226bac229c594854fbc597c9525bcb5
Narayan Waraich <narayanwaraich@gmail.com>
2019-05-23 03:00:32 -0700

    render timer

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -4,13 +4,42 @@ class StopWatch extends Component {
   constructor (props){
     super(props);
     this.state ={
-      currentDate: new Date()
+      currentMinute: 5,
+      currentSecond: 0
     }
+  }
+
+  updateTimer() {
+    let {currentMinute,currentSecond} = this.state;
+    if (currentSecond===0 && currentMinute!==0) {
+      this.setState({
+        currentMinute: currentMinute-1,
+        currentSecond: 59
+      })
+    } else if(currentSecond>0) {
+      this.setState({
+        currentMinute: currentMinute,
+        currentSecond: currentSecond-1
+      });
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.updateTimer();
+    }, 1000);
   }
 
   render(){
     return (
-      <p>StopWatch</p>
+      <div>
+        <h1>StopWatch</h1>
+        <p>
+          {this.state.currentMinute.toString().padStart(2, '0')}
+          :
+          {this.state.currentSecond.toString().padStart(2, '0')}
+        </p>
+      </div>
     );
   }
 }
